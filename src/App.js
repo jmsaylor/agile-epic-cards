054c4d955b4ce353b8bcf6cf253b3bd8855b7335
John <johnmsaylor@gmail.com>
2020-04-11 13:05:30 -0400

    adding more forms

--- src/App.js
+++ src/App.js
@@ -27,6 +27,34 @@ function App() {
       <span>
         our solution: <Form />
       </span>
+      <div>--------------Vision Board-----------------</div>
+      <span>
+        Vision Statement: <Form />
+      </span>
+      <span>
+        Target Group: <Form />
+      </span>
+      <span>
+        Needs: <Form />
+      </span>
+      <span>
+        Product: <Form />
+      </span>
+      <span>
+        Business Goals: <Form />
+      </span>
+      <span>
+        Competitors: <Form />
+      </span>
+      <span>
+        Revenue Sources: <Form />
+      </span>
+      <span>
+        Cost Factors: <Form />
+      </span>
+      <span>
+        Channels: <Form />
+      </span>
     </div>
   );
 }
